e36afc2918dcb55ed5af1b3ce513a3ae205d794f
venganza312 <75156965+venganza312@users.noreply.github.com>
2022-01-04 17:03:58 +0900

    initialize LinkedList

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,5 +1,7 @@
 package linkedlist;
 
+import java.util.Stack;
+
 public class LinkedList {
 
     /**
@@ -19,30 +21,62 @@ public class LinkedList {
 
     public static void main(String[] args) {
         LinkedList n = new LinkedList();
+
         n.append(1);
         n.append(2);
-        n.append(3);
-        n.append(4);
-        n.append(5);
-        n.retrieve();
-        n.delete(5);
+        n.append(2);
+        n.append(1);
+        n.append(1);
+
+        System.out.print("n : ");
         n.retrieve();
 
-        System.out.println("findByDataToLast : " + findByDataToLast(n.header,2).data);
-        System.out.println("findByNodeToLast : " + findByNodeToLast(n.header, 2, new Reference()).data);
+        LinkedList n2 = new LinkedList();
+        n2.append(1);
+        n2.append(2);
+        n2.append(3);
+        n2.append(4);
+
+        Node reverseNode = reverseAndClone(n.header);
+
+        while(reverseNode.next != null) {
+            System.out.print("reverseNode n -> " + reverseNode.data + " ");
+            reverseNode = reverseNode.next;
+        }
+
+        System.out.println();
+        String isEqual = isEqual(n.get(1), n2.get(1)) == true ? "isEqual" : "NotEqual";
+        System.out.println("isEqual(n, n2) : " + isEqual);
+        String isPalindrome = isPalindrome(n.get(1)) == true ? "isPalindrome" : "NotPalindrome";
+        System.out.println("isPalindrome(n) : " + isPalindrome);
+        System.out.println("findByDataToLast : " + findByDataToLast(n2.get(1), 1).data);
+        System.out.println("findByNodeToLast : " + findByNodeToLast(n2.get(1), 3, new Reference()).data);
         System.out.println("factorial : " + factorial(5));
     }
 
-    void append(int d) {
-        Node endNode = new Node();
-        endNode.data = d;
+    Node get(int index) {
         Node n = header;
+        int count = 0;
+        while(n != null) {
+            if (count == index) {
+                return n;
+            }
+            count++;
+            n = n.next;
+        }
+        return null;
+    }
+
+    void append(int d) {
+        Node n = header;
+        Node node = new Node();
+        node.data = d;
 
         while(n.next != null) {
             n = n.next;
         }
 
-        n.next = endNode;
+        n.next = node;
     }
 
     void delete(int d) {
@@ -82,9 +116,9 @@ public class LinkedList {
 
     static Node findByDataToLast(Node target, int k) {
         Node n = target;
-        int total = 1;
+        int total = 0;
 
-        while(n.next != null) {
+        while(n != null) {
             total++;
             n = n.next;
         }
@@ -99,7 +133,7 @@ public class LinkedList {
     }
 
     static class Reference {
-        public int count = 0 ;
+        public int count = 0;
     }
 
     /**
@@ -124,9 +158,35 @@ public class LinkedList {
         if (n == 1) {
             return 1;
         } else {
-            return n * factorial(n-1);
+            return n * factorial(n - 1);
         }
     }
 
+    static boolean isPalindrome(Node head) {
+        Node reversed = reverseAndClone(head);
+        return isEqual(head, reversed);
+    }
 
+    static Node reverseAndClone(Node node) {
+        Node head = null;
+        while(node != null) {
+            Node n = new Node();
+            n.data = node.data;
+            n.next = head;
+            head = n;
+            node = node.next;
+        }
+        return head;
+    }
+
+    static boolean isEqual(Node n1, Node n2) {
+        while (n1 != null && n2 != null) {
+            if (n1.data != n2.data) {
+                return false;
+            }
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return n1 == null && n2 == null;
+    }
 }
